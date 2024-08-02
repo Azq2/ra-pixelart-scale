3f3661f2a69ef7ed7fd07dd0f120c80c9cf1db8f
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2024-08-02 07:04:21 +0000

    update shaders, alpha split mode

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@ use clap::Parser;
 use librashader_runtime_gl::{FilterChainGL, GLFramebuffer, GLImage};
 use librashader_common::{Size, Viewport};
 use gl_headless::gl_headless;
-use image::GenericImageView;
-use image::ImageBuffer;
+use image::{GenericImage, DynamicImage, ImageBuffer, GenericImageView};
 use gl::types::{GLuint};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::ExitCode;
 
 /// Pixel Art scaling algorithms from RetroArch.
@@ -27,6 +26,14 @@ struct Args {
 	/// Output filename
 	#[arg(short, long)]
 	output: String,
+
+	/// Alpha saving mode: none, split
+	#[arg(short, long, default_value = "none")]
+	alpha: String,
+
+	/// Custom .slangp file
+	#[arg(long, default_value = "")]
+	custom_preset: String,
 }
 
 #[gl_headless(version = "3.3")]
@@ -35,19 +42,21 @@ unsafe fn main() -> ExitCode {
 	let shaders_dir = get_shaders_dir();
 	let shaders_index = load_shaders_index(shaders_dir.as_str());
 
-	// Load input image
-	let img = image::open(args.input.as_str()).expect("Failed to load image");
-	let (width, height) = img.dimensions();
-	let data = img.into_rgba8().into_raw();
+	let rgba_img = image::open(args.input.as_str()).expect("Failed to load image");
+	let (width, height) = rgba_img.dimensions();
 
-	let shader_filename = shaders_index[args.method.clone()][1].as_str();
-	if shader_filename.is_none() {
-		eprintln!("Scaling method {} not found.", args.method);
-		return ExitCode::from(1);
+	let mut preset_path = PathBuf::from(args.custom_preset.as_str());
+	let mut default_scale = 1f64;
+
+	if args.custom_preset == "" {
+		let preset_filename = shaders_index[args.method.clone()][1].as_str();
+		if preset_filename.is_none() {
+			eprintln!("Scaling method {} not found.", args.method);
+			return ExitCode::from(1);
+		}
+		preset_path = Path::new(shaders_dir.as_str()).join(preset_filename.unwrap().to_string());
+		default_scale = shaders_index[args.method.clone()][0].as_f64().unwrap();
 	}
-
-	let default_scale = shaders_index[args.method.clone()][0].as_f64().unwrap();
-	let shader_path = Path::new(shaders_dir.as_str()).join(shader_filename.unwrap().to_string());
 
 	let mut output_scale = args.scale;
 	if args.scale <= 0.0 {
@@ -59,7 +68,7 @@ unsafe fn main() -> ExitCode {
 		((height as f64) * output_scale).round() as u32
 	);
 
-	eprintln!("Shader: {}", shader_path.to_str().unwrap());
+	eprintln!("Preset: {}", preset_path.to_str().unwrap());
 	eprintln!("Input: {}", args.input);
 	eprintln!("Input size: {}x{}", width, height);
 	eprintln!("Output: {}", args.output);
@@ -70,9 +79,6 @@ unsafe fn main() -> ExitCode {
 	let (_rendered_framebuffer, rendered_texture) = create_texture(width, height);
 	let (output_framebuffer, output_texture) = create_texture(output_size.width, output_size.height);
 
-	gl::BindTexture(gl::TEXTURE_2D, rendered_texture);
-	gl::TexSubImage2D(gl::TEXTURE_2D, 0, 0, 0, width as _, height as _, gl::RGBA, gl::UNSIGNED_BYTE, data.as_ptr() as _);
-
 	let rendered = GLImage {
 		handle: rendered_texture,
 		format: gl::RGBA8,
@@ -81,25 +87,56 @@ unsafe fn main() -> ExitCode {
 			height: height,
 		},
 	};
-
-	let output = GLFramebuffer::new_from_raw(output_texture, output_framebuffer, gl::RGBA8, output_size, 1);
-
 	let viewport = Viewport {
 		x: 0f32,
 		y: 0f32,
-		output: &output,
+		output: &GLFramebuffer::new_from_raw(output_texture, output_framebuffer, gl::RGBA8, output_size, 1),
 		mvp: None,
 	};
 
-	gl::BindFramebuffer(gl::FRAMEBUFFER, 0);
-
-	let mut filter = FilterChainGL::load_from_path(shader_path.to_str().unwrap(), None).unwrap();
-	filter.frame(&rendered, &viewport, 0, None).unwrap();
-
-	gl::BindFramebuffer(gl::FRAMEBUFFER, output_framebuffer);
-	render_as_png(args.output.as_str(), output_size);
-
+	let mut filter = FilterChainGL::load_from_path(preset_path.to_str().unwrap(), None).unwrap();
+	if args.alpha == "split" {
+		let (rgb_img, alpha_img) = split_alpha(&rgba_img);
+		let rgb_scaled = process_image(&mut filter, &rgb_img, &rendered, &viewport, output_size, output_framebuffer);
+		let alpha_scaled = process_image(&mut filter, &alpha_img, &rendered, &viewport, output_size, output_framebuffer);
+		let rgba_scaled = merge_alpha(&rgb_scaled, &alpha_scaled);
+		rgba_scaled.save(args.output).expect("Image save() error");
+	} else if args.alpha == "none" {
+		let rgba_scaled = process_image(&mut filter, &rgba_img, &rendered, &viewport, output_size, output_framebuffer);
+		rgba_scaled.save(args.output).expect("Image save() error");
+	} else {
+		panic!("Invalid alpha mode: {}", args.alpha);
+	}
 	return ExitCode::from(0);
+}
+
+unsafe fn process_image(
+	filter: &mut FilterChainGL,
+	img: &DynamicImage,
+	rendered: &GLImage,
+	viewport: &Viewport<&GLFramebuffer>,
+	output_size: Size<u32>,
+	fbo: GLuint
+) -> DynamicImage {
+	let data = img.clone().into_rgba8().into_raw();
+	gl::BindTexture(gl::TEXTURE_2D, rendered.handle);
+	gl::TexSubImage2D(gl::TEXTURE_2D, 0, 0, 0, rendered.size.width as _, rendered.size.height as _, gl::RGBA, gl::UNSIGNED_BYTE, data.as_ptr() as _);
+
+	filter.frame(&rendered, &viewport, 0, None).expect("Can't process shader.");
+
+	let mut tmp_buffer = vec![0u8; (output_size.width * output_size.height * 4) as usize];
+	gl::BindFramebuffer(gl::FRAMEBUFFER, fbo);
+	gl::ReadPixels(
+		0,
+		0,
+		output_size.width as _,
+		output_size.height as _,
+		gl::RGBA,
+		gl::UNSIGNED_BYTE,
+		tmp_buffer.as_mut_ptr() as _,
+	);
+	let buffer: ImageBuffer<image::Rgba<u8>, Vec<u8>> = ImageBuffer::from_raw(output_size.width, output_size.height, tmp_buffer).unwrap();
+	return DynamicImage::from(buffer);
 }
 
 fn load_shaders_index(shaders_dir: &str) -> serde_json::Value {
@@ -121,19 +158,40 @@ fn get_shaders_dir() -> String {
 	}
 }
 
-unsafe fn render_as_png(file: &str, size: Size<u32>) {
-	let mut data = vec![0u8; (size.width * size.height * 4) as usize];
-	gl::ReadPixels(
-		0,
-		0,
-		size.width as _,
-		size.height as _,
-		gl::RGBA,
-		gl::UNSIGNED_BYTE,
-		data.as_mut_ptr() as _,
-	);
-	let buffer: ImageBuffer<image::Rgba<u8>, Vec<u8>> = ImageBuffer::from_raw(size.width, size.height, data).unwrap();
-	buffer.save(file).expect("Image saving failed");
+fn split_alpha(input_img: &DynamicImage) -> (DynamicImage, DynamicImage) {
+	let rgba_img = input_img.to_rgba8();
+
+	let (width, height) = rgba_img.dimensions();
+	let mut rgb_img = DynamicImage::new(width, height, image::ColorType::Rgb8);
+	let mut alpha_img = DynamicImage::new(width, height, image::ColorType::Rgb8);
+
+	for y in 0..height {
+		for x in 0..width {
+			let pixel = rgba_img.get_pixel(x, y);
+			let rgb_pixel = image::Rgba([pixel[0], pixel[1], pixel[2], 255]);
+			let alpha_pixel = image::Rgba([pixel[3], pixel[3], pixel[3], 255]);
+			rgb_img.put_pixel(x, y, rgb_pixel);
+			alpha_img.put_pixel(x, y, alpha_pixel);
+		}
+	}
+
+	return (rgb_img, alpha_img);
+}
+
+fn merge_alpha(rgb_img: &DynamicImage, alpha_img: &DynamicImage) -> DynamicImage {
+	let (width, height) = rgb_img.dimensions();
+	let mut rgba_img = DynamicImage::new(width, height, image::ColorType::Rgba8);
+
+	for y in 0..height {
+		for x in 0..width {
+			let pixel = rgb_img.get_pixel(x, y);
+			let alpha = alpha_img.get_pixel(x, y);
+			let rgba_pixel = image::Rgba([pixel[0], pixel[1], pixel[2], alpha[0]]);
+			rgba_img.put_pixel(x, y, rgba_pixel);
+		}
+	}
+
+	return rgba_img;
 }
 
 unsafe fn create_texture(width: u32, height: u32) -> (GLuint, GLuint) {
